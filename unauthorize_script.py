84e3c830a8e38b5165979815c08396fbc3cda6f6
TaiwolfB <tailupbogdan@gmail.com>
2023-05-12 12:47:26 +0300

    supervisor logs

--- unauthorize_script.py
+++
@@ -1,7 +0,0 @@
-import sys
-import json
-import subprocess
-
-if __name__ == '__main__':
-    LOGGED_IN_USER = sys.argv[1]
-    subprocess.check_output(f'az logout --username {LOGGED_IN_USER}', shell=True).decode('utf-8')
